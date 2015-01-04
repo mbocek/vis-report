b9358dcafcc4c102b0a2f6f1bf85719f563c608b
Michal Bocek <michal.bocek@gmail.com>
2015-01-04 14:50:19 +0100

    first version of vis reporting

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+VFP driver:
+http://download.microsoft.com/download/vfoxodbcdriver/Install/6.1/W9XNT4/EN-US/VFPODBC.msi
+
+Use java 32bit (odbc driver is 32bit version)
---
+++ export.groovy
@@ -0,0 +1,132 @@
+/**
+ * Generating report from VIS database.
+ * Database is Visual foxpro.
+ */
+
+import groovy.sql.Sql
+
+import java.sql.Date as SqlDate
+import java.sql.SQLException
+
+import jxl.*
+import jxl.write.*
+
+// Parameters to report. Limits for time period.
+def fromString = '2014-12-01'
+def toString = '2014-12-31'
+def dateFrom = Date.parse('yyyy-MM-dd', fromString)
+def dateTo = Date.parse('yyyy-MM-dd', toString)
+
+println "Running report for period: ${dateFrom.format('dd.MM.yyyy')}-${dateTo.format('dd.MM.yyyy')}"
+
+// dsn name in odbc 32bit windows setup
+def dsn = "vis-firmy"
+
+def from = new SqlDate(dateFrom.getTime())
+def to = new SqlDate(dateTo.getTime())
+
+// report path - can be full path or relative path 
+def outputFilePath = "report-${fromString}-${toString}.xls"
+def ws = new WorkbookSettings()
+ws.setEncoding("cp1250")
+def workbook = Workbook.createWorkbook(new File(outputFilePath), ws)
+
+// date format in excel
+def customDateFormat = new DateFormat("dd.MM.yyyy"); 
+dateFormat = new WritableCellFormat (customDateFormat); 
+
+// database charset setup
+def prop = new java.util.Properties();
+prop.put("charSet", "cp1250");
+
+// connection string to jdbc odbc bridge
+def sql = Sql.newInstance("jdbc:odbc:${dsn}", prop, "sun.jdbc.odbc.JdbcOdbcDriver")
+
+// query to get all persons in system
+def queryAllPersons = "select * from stravnik"
+// query to get all orders between period and for specified person
+def queryDetail = "select ob.datum, ob.druh, ob.ev_cislo, ob.pocet, ji.nazev, ji.cena1 " + 
+            "from objednav as ob, jidelnic as ji " + 
+            "where ob.datum between :fromDate and :toDate " + 
+            "and ji.datum = ob.datum " +
+            "and ji.druh = ob.druh " +
+            "and ob.ev_cislo = :evCislo " + 
+            "order by ob.datum asc"
+// query to get summary for orders between period and for specified person
+def querySum = "select ji.cena1, sum(ob.pocet) as pocet " + 
+            "from objednav as ob, jidelnic as ji " + 
+            "where ob.datum between :fromDate and :toDate " + 
+            "and ji.datum = ob.datum " +
+            "and ji.druh = ob.druh " +
+            "and ob.ev_cislo = :evCislo " + 
+            "group by ji.cena1"
+            
+
+sql.eachRow(queryAllPersons) { stravnik ->
+    def evCislo = stravnik.ev_cislo
+    def sheet = workbook.createSheet(stravnik.jmeno, evCislo.intValue())
+    sheet.addCell(new Label(0, 0, "Datum")) 
+    sheet.addCell(new Label(1, 0, "Druh")) 
+    sheet.addCell(new Label(2, 0, "J\u00EDdlo")) 
+    sheet.addCell(new Label(3, 0, "Po\u010Det"))
+    sheet.addCell(new Label(4, 0, "Cena"))
+    sheet.addCell(new Label(5, 0, "Suma"))
+    
+    try {
+        int row = 0
+        sql.eachRow(queryDetail, [fromDate: from, toDate: to, evCislo: evCislo]) {
+            int col = 0
+            row++
+            int formulaRow = row+1
+            sheet.addCell(new DateTime(col++, row, it.datum, dateFormat))
+            sheet.addCell(new Label(col++, row, it.druh))
+            sheet.addCell(new Label(col++, row, it.nazev))
+            sheet.addCell(new Number(col++, row, it.pocet))
+            sheet.addCell(new Number(col++, row, it.cena1))
+            //Create a formula for adding cells
+            Formula sum = new Formula(col++, row, "D" + formulaRow + "*E" + formulaRow);
+            sheet.addCell(sum);
+        }
+        
+        if (row > 0) {
+            ++row
+            sheet.mergeCells(0, row, 4, row);
+            sheet.addCell(new Label(0, row, "Celkova suma:"))
+            Formula sum = new Formula(5, row, "SUM(F2:F" + row + ")")
+            sheet.addCell(sum)
+            
+            int startRow = row += 5
+            sql.eachRow(querySum, [fromDate: from, toDate: to, evCislo: evCislo]) {
+                row++
+                int formulaRow = row+1
+                sheet.mergeCells(0, row, 2, row)
+                sheet.addCell(new Label(0, row, "Cena za j\u00EDdlo a po\u010Det kus\u016F:"))
+                sheet.addCell(new Number(3, row, it.pocet))
+                sheet.addCell(new Number(4, row, it.cena1))
+                sum = new Formula(5, row, "D" + formulaRow + "*E" + formulaRow)
+                sheet.addCell(sum)
+            }
+            
+            ++startRow
+            ++row
+            sheet.mergeCells(0, row, 4, row);
+            sheet.addCell(new Label(0, row, "Celkova suma:"))
+            sum = new Formula(5, row, "SUM(F" + startRow + ":F" + row + ")");
+            sheet.addCell(sum);
+        }
+        
+    } catch (SQLException e) {
+        println "No data found for ev_cislo: ${evCislo}"
+    }
+    
+    for(int x = 0; x < 5; x++) {
+        cell=sheet.getColumnView(x);
+        cell.setAutosize(true);
+        sheet.setColumnView(x, cell);
+    }    
+}
+
+workbook.write(); 
+workbook.close();
+
+println "Report generated."
