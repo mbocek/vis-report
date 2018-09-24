02b97e21a5b9f636ee4b9a12753f7095f14423fa
Lucie Petrášková <lucie.petraskova@EKOLANDIA.LAN>
2018-09-24 22:11:32 +0200

    fixed dsn name for firmy

--- bin/export-firmy-previous-month.bat
+++ bin/export-firmy-previous-month.bat
@@ -5,7 +5,7 @@ rem and redefine variables in this file.
 if exist env.bat @call env.bat
 set SCRIPT_CLASS_PATH=../lib/jxl.jar
 set SCRIPT_PATH=../script/export.groovy
-set SCRIPT_DATA_SOURCE=vis-skoly
+set SCRIPT_DATA_SOURCE=vis-firmy
 set SCRIPT_MONTH_SHIFT=-1
 
 %GROOVY_HOME%\bin\groovy -cp %SCRIPT_CLASS_PATH% %SCRIPT_PATH% -s %SCRIPT_DATA_SOURCE% -m %SCRIPT_MONTH_SHIFT%
